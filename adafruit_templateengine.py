024309a0d9c5204946b339a25aff21ebf03bc298
foamyguy <foamyguy@gmail.com>
2023-09-24 12:44:22 -0500

    initial commit

---
+++ adafruit_templateengine.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 MichalPokusa, TimCocks for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_templateengine`
+================================================================================
+
+Templating engine to substitute variables into a template string.
+Templates can also include conditional logic and loops. Often used for web pages.
+
+
+* Author(s): MichalPokusa, TimCocks
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+Any CircuitPython microcontroller.
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TemplateEngine.git"
